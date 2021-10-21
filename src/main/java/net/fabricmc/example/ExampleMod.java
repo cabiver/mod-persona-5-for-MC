215ed538b82f4f43fcba3d77fa1a010d84f95bbd
cabiver <carlosvergel01@gmail.com>
2021-10-20 20:23:18 -0500

    mask joker

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,21 +1,19 @@
 package net.fabricmc.example;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.item.Item;
 import net.fabricmc.api.ModInitializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import net.minecraft.item.ItemGroup;
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("modid");
+	public static final Item MUD_BALL = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("persona","arsend"), MUD_BALL);
 	}
+
 }
+
---
+++ src/main/java/net/fabricmc/models/maskPersona.java
@@ -0,0 +1,17 @@
+// package net.fabricmc.models;
+
+// import net.minecraft.item.Item;
+// import net.minecraft.item.ItemGroup;
+// import org.apache.logging.log4j.LogManager;
+// import org.apache.logging.log4j.Logger;
+
+// public class maskPersona {
+// 	public static final Item MASK = new Item(new Item.Settings().group(ItemGroup.MISC));
+
+	
+// 	@Override
+// 	public void maskPersona() {
+// 		Registry.register(Registry.ITEM, new Identifier("tutorial",'mud_ball', MASK));
+// 	}
+// }
+
